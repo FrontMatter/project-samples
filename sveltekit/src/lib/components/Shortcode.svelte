4cd13b9a9ea592000a8fb51a40e6e1226c0c9d13
Bryan Klein <zivbk1@gmail.com>
2022-01-14 22:58:24 +0000

    Added example content and updated configs.

---
+++ sveltekit/src/lib/components/Shortcode.svelte
@@ -0,0 +1,10 @@
+<script>
+  let variableName = "This is today's date and time:"
+  let currentDate = new Date();
+  export let passedIn = ""
+</script>
+
+<div class="text-green-300">
+  {variableName}: {currentDate.toLocaleString()}<br/>
+  <span class="text-blue-300">And this is the string passed into this component, <strong class="text-indigo-600">"{passedIn}"</strong>.</span>
+</div>
